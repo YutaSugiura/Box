15a375e484fa7f5c76d32ab72ea95e6f499f7639
YutaSugiura <anoter9room@gmail.com>
2015-06-03 11:05:34 +0900

    Update TextBox.cpp
    関数の追加
    コメントの追加

--- TextBox.cpp
+++ TextBox.cpp
@@ -5,6 +5,8 @@ USING_NS_CC;
 
 TextBox::TextBox()
 :m_text()
+, m_x(0)
+, m_y(0)
 {
 }
 
@@ -21,11 +23,11 @@ bool TextBox::init()
 	TTFConfig ttf("fonts/ipaexm.ttf", 42);
 	//"__"を表示
 	m_text = Label::createWithTTF(ttf, "__");
-	
+	//座標を指定
+	m_text->setPosition(0,0);
+	//addChild
 	this->addChild(m_text);
-
-
-
+	
 	return true;
 }
 
@@ -36,14 +38,14 @@ void TextBox::Update()
 
 }
 
-void TextBox::SetText(char* text)
+void TextBox::m_SetText(char* text)
 {
-	removeAllChildren();
-	//フォントを指定
-	TTFConfig ttf("fonts/ipaexm.ttf", 42);
-	//表示
-	m_text = Label::createWithTTF(ttf, text);
-	//表示する座標の設定
-	m_text->setPosition(Vec2(250, 390));
-	this->addChild(m_text);
+	m_text->setString(text);
+	m_text->setPosition(m_x, m_y);
+}
+
+void TextBox::m_SetPos(cocos2d::Vec2 pos)
+{
+	m_x = pos.x;
+	m_y = pos.y;
 }
