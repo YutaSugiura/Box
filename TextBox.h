4349bb02ff013f54f613549cb03ebc397f9aaa1a
YutaSugiura <anoter9room@gmail.com>
2015-06-03 11:02:14 +0900

    Update TextBox.h
    コメント修正
    関数名の変更
    表示座標を指定する関数を追加

--- TextBox.h
+++ TextBox.h
@@ -6,7 +6,8 @@ class TextBox :public  cocos2d::Layer
 private:
 
 	cocos2d::Label* m_text;
-
+	int m_x;
+	int m_y;
 
 public:
 
@@ -21,9 +22,12 @@ public:
 	bool init();
 
 	void Update();
-
-	void SetText(char* text);
-
+	
+	//表示したいテキストを入れる
+	void m_SetText(char* text);
+	
+	//表示座標を指定
+	void m_SetPos(cocos2d::Vec2 pos);
 	CREATE_FUNC(TextBox);
 
 };
